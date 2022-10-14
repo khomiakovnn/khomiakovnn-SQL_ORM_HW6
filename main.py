22f2f3c9f5ae4d72da3478d296ecad41089cfcbe
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2022-10-14 20:16:20 +0300

    final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
 import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 
 Base = declarative_base()
@@ -46,10 +47,52 @@ class Stock(Base):
     id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
-    
+
 def create_tables(engine):
-    Base.metadata.drop_all(engine)
+    Base.metadata.drop_all(engine)  # для очистки таблиц при отладке кода
     Base.metadata.create_all(engine)
+
+
+def insert_data(file, session):
+    """Insert test data from json file"""
+
+    with open(file) as file:
+        data = json.load(file)
+
+    for line in data:
+        if line['model'] == "publisher":
+            row = Publisher(
+                id=line['pk'],
+                name=line['fields']['name']
+            )
+        elif line['model'] == "book":
+            row = Book(
+                id=line['pk'],
+                title=line['fields']['title'],
+                id_publisher=line['fields']['id_publisher']
+            )
+        elif line['model'] == "shop":
+            row = Shop(
+                id=line['pk'],
+                name=line['fields']['name']
+            )
+        elif line['model'] == "stock":
+            row = Stock(
+                id=line['pk'],
+                id_book=line['fields']['id_book'],
+                id_shop=line['fields']['id_shop'],
+                count=line['fields']['count']
+            )
+        else:
+            row = Sale(
+                id=line['pk'],
+                price=line['fields']['price'],
+                date_sale=line['fields']['date_sale'],
+                id_stock=line['fields']['id_stock'],
+                count=line['fields']['count']
+            )
+        session.add(row)
+        session.commit()
 
 
 def get_user_data():
@@ -60,15 +103,38 @@ def get_user_data():
     dns_list = []
     for data in userdata:
         dns_list.append(data[(data.find('=') + 2):-1])
-    DSN = f"postgresql://{dns_list[3]}:{dns_list[4]}@{dns_list[1]}:{dns_list[2]}/{dns_list[0]}"
-    return DSN
+    result = (
+        f"postgresql://{dns_list[3]}:{dns_list[4]}@"
+        f"{dns_list[1]}:{dns_list[2]}/{dns_list[0]}"
+    )
+    return result
+
+
+def query_db(session, id):
+    """Query from publisher table"""
+
+    publisher_query = session.query(Publisher).filter(Publisher.id == id)
+    for s in publisher_query.all():
+        print(s.id, s.name)
 
 
 def main():
     """Main function"""
 
-    engine = sq.create_engine(get_user_data())
-    create_tables(engine)
+    test_data_file = 'tests_data.json'
+    DSN = get_user_data()
+    engine = sq.create_engine(DSN)
+    create_tables(engine)  # Задание 2 (подключение и импорт)
+    print('Таблицы созданы.')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    insert_data(test_data_file, session)  # Задание 3
+    print(f"Таблицы заполнены тестовыми данными '{test_data_file}'")
+
+    # publish = input('Введите данны издателя: ') - sublime не разрешает input, ввод задан жестко в следующей строке
+    publish = 1
+    query_db(session, publish)  # Задание 2 (издатель)
 
 
 if __name__ == '__main__':
